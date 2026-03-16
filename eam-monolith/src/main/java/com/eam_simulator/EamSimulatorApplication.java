ac8c6847606dfb4e28b528cd5dd4c2342d54d448
Adam Zimny <adamczyk97@op.pl>
2026-03-16 01:02:33 +0100

    Add starter application class and test for running app

---
+++ eam-monolith/src/main/java/com/eam_simulator/EamSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.eam_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EamSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EamSimulatorApplication.class, args);
+	}
+
+}
---
+++ eam-monolith/src/test/java/com/eam_simulator/EamSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EamSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
