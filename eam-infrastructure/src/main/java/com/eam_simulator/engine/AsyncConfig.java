9be5fcc4cde60eed2458cecfadafcd7fc432d9d1
Adam Zimny <adamczyk97@op.pl>
2026-03-19 23:31:37 +0100

    Add configuration for Async threading

---
+++ eam-infrastructure/src/main/java/com/eam_simulator/engine/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.eam_simulator.engine;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+class AsyncConfig {
+    @Bean(name = "gameEngineTaskExecutor")
+    Executor gameEngineTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("GameEngine-");
+        executor.initialize();
+        return executor;
+    }
+}
