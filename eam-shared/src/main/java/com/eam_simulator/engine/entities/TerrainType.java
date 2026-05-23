b9ffa91556b3d06d045c920a872fd5376a311a0b
Adam Zimny <adamczyk97@op.pl>
2026-05-23 20:14:34 +0200

    Adding enumeration types, resources and transitions

---
+++ eam-shared/src/main/java/com/eam_simulator/engine/entities/TerrainType.java
@@ -0,0 +1,14 @@
+package com.eam_simulator.engine.entities;
+
+public enum TerrainType {
+    GRASS,
+    UNEVEN_GROUND,
+    WATER,
+    SHALLOW_WATER,
+    MOUNTAIN,
+    SINKHOLE,
+    HILL,
+    SNOW,
+    SAND,
+    FOREST_FLOOR
+}
