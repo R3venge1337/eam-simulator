87a7ce79b88f43463a285f3a3c6ff8a7de4bfff0
Adam Zimny <adamczyk97@op.pl>
2026-06-06 01:37:24 +0200

    Change path and add elevation, walkable, passageType to MapTile

--- eam-application/src/main/java/com/eam_simulator/map/dto/TileView.java
+++ eam-application/src/main/java/com/eam_simulator/map/dto/TileView.java
@@ -1,6 +1,11 @@
 package com.eam_simulator.map.dto;
 
-import com.eam_simulator.engine.entities.Coordinates;
+import com.eam_simulator.domain.map.entities.Coordinates;
+import com.eam_simulator.domain.map.entities.PassageType;
+import com.eam_simulator.domain.map.entities.TerrainType;
 
-public record TileView(Coordinates coords) {
+public record TileView(Coordinates coords, TerrainType terrain,
+                       int elevation,
+                       PassageType passageType,
+                       boolean walkable) {
 }
