79752fd70df336055b7e36caf693edd890facc23
Adam Zimny <adamczyk97@op.pl>
2026-06-06 01:46:58 +0200

    Add to generateInitialWOrld method generators with settings and context

--- eam-application/src/main/java/com/eam_simulator/map/InitialBootstrapMapService.java
+++ eam-application/src/main/java/com/eam_simulator/map/InitialBootstrapMapService.java
@@ -1,28 +1,59 @@
 package com.eam_simulator.map;
 
-import com.eam_simulator.map.dto.CreateMapCommand;
-import com.eam_simulator.map.dto.MapDimension;
-import com.eam_simulator.map.dto.MapView;
-import com.eam_simulator.map.dto.TileView;
+import com.eam_simulator.map.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RequiredArgsConstructor
-public class InitialBootstrapMapService implements MapFacade {
+class InitialBootstrapMapService implements MapFacade {
 
     private final GameMapRepository gameMapRepository;
     private final ApplicationEventPublisher springEventPublisher;
+    private final MapEnvironmentService environmentService;
+    private final List<MapGenerationStep> pipeline;
 
     @Override
     @Transactional
     public MapView generateInitialWorld(CreateMapCommand command) {
         GameMap gameMap = new GameMap(
                 new MapName(command.mapName()),
-                new MapSize(command.size().width(),command.size().height())
+                new MapSize(command.size().width(), command.size().height())
         );
+
+        List<Object> activeConfigs = new ArrayList<>();
+        GenerationSettings settings = command.settings();
+
+        activeConfigs.add(settings.hillSettings());
+        activeConfigs.add(settings.lakesSettings());
+        activeConfigs.add(settings.mountainSettings());
+        activeConfigs.add(settings.sandSettings());
+
+
+        if (settings.hillSettings().enableHills()) {
+            activeConfigs.add(new HillGenerationContext(true, settings.hillSettings().maxHillPercentage()));
+        }
+
+        if (settings.lakesSettings().enableLakes()) {
+            activeConfigs.add(new LakesGenerationContext(true, settings.lakesSettings().numberOfLakes(), 0.30));
+        }
+
+        if (settings.mountainSettings().enableMountains()) {
+            activeConfigs.add(new MountainGenerationContext(true, settings.mountainSettings().numberOfRanges(), settings.mountainSettings().maxMountainPercentage()));
+        }
+
+        if (settings.sandSettings().enableBeach()) {
+            activeConfigs.add(new SandGenerationContext());
+        }
+
+        environmentService.generate(gameMap, this.pipeline, activeConfigs);
+
         GameMap savedMap = gameMapRepository.save(gameMap);
+
         gameMap.pullEvents().forEach(springEventPublisher::publishEvent);
 
         int width = savedMap.getSize().width();
@@ -32,7 +63,7 @@ public class InitialBootstrapMapService implements MapFacade {
 
         return new MapView(
                 savedMap.getMapName().name(),
-                new MapDimension(command.size().width(),command.size().height()),
+                new MapDimension(command.size().width(), command.size().height()),
                 viewGrid
         );
     }
@@ -41,7 +72,7 @@ public class InitialBootstrapMapService implements MapFacade {
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 Tile domainTile = domainGrid[x][y];
-                viewGrid[x][y] = new TileView(domainTile.getCoords());
+                viewGrid[x][y] = new TileView(domainTile.getCoords(), domainTile.getTerrain(), domainTile.getElevation(), domainTile.getPassage(), domainTile.isWalkable());
             }
         }
     }
